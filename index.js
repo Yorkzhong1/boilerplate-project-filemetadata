4eca649231c673c80218b7b17f05b2198928c2ea
Yorkzhong1 <yorkzhong@gmail.com>
2023-10-31 21:29:19 -0600

    c

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var app = express();
 var cors = require('cors');
 require('dotenv').config()
+let bodyParser = require('body-parser')
+const multer = require('multer')
 
-var app = express();
+
+const upload = multer({ dest: 'uploads/'});
+
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +17,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  console.log('uploading file...')
+  res.json({
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size
+  })
+  // res.json({filename:req.file.filename})
+})
 
 
 
